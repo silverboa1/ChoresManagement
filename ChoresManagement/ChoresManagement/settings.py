70b173b94b8000f9943a1097d8e448c7a0c63dcf
silverboa1 <maksym_maznychka@ukr.net>
2025-08-03 13:27:54 +0300

    accouts app setup

--- ChoresManagement/ChoresManagement/settings.py
+++ ChoresManagement/ChoresManagement/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "accounts"
 ]
 
 MIDDLEWARE = [
@@ -110,6 +111,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = "accounts.User"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
