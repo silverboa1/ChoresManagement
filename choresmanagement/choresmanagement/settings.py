f0ae609f70403adf2b7ce3d572aa55effb9fe484
silverboa1 <maksym_maznychka@ukr.net>
2025-09-14 13:18:25 +0300

    setup templates and css

--- choresmanagement/choresmanagement/settings.py
+++ choresmanagement/choresmanagement/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'choresmanagement.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,8 +114,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Папка для збору статичних файлів у продакшені
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+# Папки, де Django шукає статичні файли під час розробки
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+# Media files (користувацькі завантаження)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media' 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
